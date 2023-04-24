36c049d0a9e3d6826812442ff28cd9cb76fd8b7e
Ajith shetty <ajith.shettyy@gmail.com>
2023-04-25 00:06:07 +0530

    payment app

--- api/payment.ts
+++ api/payment.ts
@@ -3,34 +3,24 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY;
 
-import Stripe from "stripe";
-const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-  typescript: true,
-});
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+// import Stripe from "stripe";
+// const stripe = new Stripe(STRIPE_SECRET_KEY, {
+//   apiVersion: "2022-11-15",
+//   typescript: true,
+// });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { body } = req;
-  const name = "Ajith";
-  const email =  "ajith.shettyy@gmail.com";
-  const amount = 1;
-  
   try {
-    stripe.customers
-      .create({
-        name,
-        email,
-        source: STRIPE_PUBLISHABLE_KEY
-      })
-      .then(customer =>
-        stripe.charges.create({
-          amount: amount * 100,
-          currency: "inr",
-          customer: customer.id
-        })
-      )
-      .then((paymentDone) => res.json(paymentDone))
-      .catch(err => console.log(err));
+    const token = await stripe.tokens.create({
+      card: {
+        number: "4242424242424242",
+        exp_month: 4,
+        exp_year: 2024,
+        cvc: "314",
+      },
+    });
+    res.json(token);
   } catch (err) {
     res.send(err);
   }
