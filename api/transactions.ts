2a3dbc5b9929be973cbbeea99646a9406f3a7756
Ajith shetty <ajith.shettyy@gmail.com>
2023-04-25 11:02:16 +0530

    transactions list

--- api/transactions.ts
+++ api/transactions.ts
@@ -4,8 +4,6 @@ const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
-const API_BASE = "https://nodejs-serverless-function-express-psi-two.vercel.app/api/"
-
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const transactions = await stripe.balanceTransactions.list(
