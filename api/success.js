ab5d2c7b944f5f499f8c77021d91f88878a47e3a
Ajith shetty <ajith.shettyy@gmail.com>
2023-04-25 12:50:36 +0530

    success html

--- api/success.js
+++ api/success.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
+  // Handle post success transactions
+  res.send("Payment Successfull");
   return res.redirect(302, "https://react-payment-app.vercel.app/")
 }
