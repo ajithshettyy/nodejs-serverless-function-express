0cd9deb7d8d3ea8c391c4f9f63bc7efbc872a9c4
Ajith shetty <ajith.shettyy@gmail.com>
2023-04-25 12:57:06 +0530

    success html

--- api/success.js
+++ api/success.js
@@ -1,8 +1,9 @@
+const APP_LINK = "https://react-payment-app.vercel.app/";
 export default function handler(req, res) {
   // Handle post success transactions
   return res.send(`<div style="text-align: center;margin-top: 10%;">
  <h1>Payment Successful</h1>
- <h1>Go back to <a href="">App</a></h1>
+ Go back to <a href="${APP_LINK}">App</a>
  </div>`);
   //return res.redirect(302, "https://react-payment-app.vercel.app/")
 }
